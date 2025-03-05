d2fae0d80656a552a44f581fac35a3e3006d99a1
qus0in <qus0in@gmail.com>
2025-03-05 10:47:50 +0900

    stream, map, toList

---
+++ src/Solution02.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Solution02 {
+    public static void main(String[] args)
+    {
+        Logger logger = Logger.getLogger("Stream");
+        // 배열이 아닌 자료구조의 경우 제너릭을 쓰기 때문에
+        // Wrapper 클래스가 필요하다
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        logger.info(numbers.toString());
+        logger.info(numbers.stream().map(x -> x * 2).toList().toString()); // .stream, .toList
+        ;
+    }
+}
+

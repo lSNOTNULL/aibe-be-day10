85c520ce7f988216b1df1ea153364b4f75c798cb
qus0in <qus0in@gmail.com>
2025-03-05 10:51:08 +0900

    여기까지

--- src/Solution02.java
+++ src/Solution02.java
@@ -11,7 +11,5 @@ public class Solution02 {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         logger.info(numbers.toString());
         logger.info(numbers.stream().map(x -> x * 2).toList().toString()); // .stream, .toList
-        ;
     }
 }
-

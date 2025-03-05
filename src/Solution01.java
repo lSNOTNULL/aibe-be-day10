cd34a6469173495bb951d578d56ee6d9445bad17
qus0in <qus0in@gmail.com>
2025-03-05 10:33:46 +0900

    lambda base

--- src/Solution01.java
+++ src/Solution01.java
@@ -5,17 +5,42 @@ public class Solution01 {
         ICalculator calculator = new Calculator();
         Logger logger = Logger.getLogger("calculator");
         logger.info("%d".formatted(calculator.add(1, 2)));
+        logger.info("%d".formatted(calculator.subtract(1, 2)));
+        // 피연산자 2개 -> 계산 -> 그 때 그 때? -> 매개변수와 반환값만 인터페이스 지정하고...
+        // 함수는 그때그때 익명함수로 못 넣어?
+//        ICalculatorLambda calculatorLambda = (a, b) -> a * b;
+        ICalculatorLambda calculatorLambda = (a, b) -> a % b;
+        logger.info("%d".formatted(calculatorLambda.run(5, 12)));
+        ICalculatorLambda calculatorLambda2 = calculator::add;
+        logger.info("%d".formatted(calculatorLambda2.run(5, 12)));
+        ICalculatorLambda calculatorLambda3 = Calculator::pythagoras;
+        logger.info("%d".formatted(calculatorLambda3.run(5, 12)));
     }
+}
+
+@FunctionalInterface
+interface ICalculatorLambda {
+    int run(int a, int b);
 }
 
 class Calculator implements ICalculator {
 
+    public static int pythagoras(int a, int b) {
+        return (int) Math.pow(Math.pow(a, 2) + Math.pow(b, 2), 0.5);
+    }
+
     @Override
     public int add(int a, int b) {
-        return 0;
+        return a + b;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
 
 interface ICalculator {
     int add(int a, int b);
+    int subtract(int a, int b);
 }
